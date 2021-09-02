047c90501b58df0c5d62a7e06be6560048bf74db
TeamCodeCreator <potyuk@ukr.net>
2021-09-02 22:05:42 +0300

    task number_1_inital

---
+++ src/YouCantCodeUnderPressureNumber1Demo.java
@@ -0,0 +1,15 @@
+class Java {
+    public static int doubleInteger(int i) {
+        // Double the integer and return it!
+        return i*2;
+    }
+}
+
+
+
+public class YouCantCodeUnderPressureNumber1Demo {
+    public static void main(String[] args) {
+        // Testing
+        System.out.println(Java.doubleInteger(10));
+    }
+}
